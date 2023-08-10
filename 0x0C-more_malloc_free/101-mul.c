4ff40510aaef90df452867a849ede7cae17f13bc
Mirimo1967 <vivmirimo@gmail.com>
2023-08-10 04:10:15 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -6,38 +6,38 @@
 
 /**
  * is_digit - checks if a string contains a non-digit char
- * @n: string to be evaluated
+ * @s: string to be evaluated
  *
  * Return: 0 if a non-digit is found, 1 otherwise
  */
-int is_digit(char *x)
+int is_digit(char *s)
 {
-	int x = 0;
+	int i = 0;
 
-	while (n[x])
+	while (s[i])
 	{
-		if (n[x] < '0' || n[x] > '9')
+		if (s[i] < '0' || s[i] > '9')
 			return (0);
-		x++;
+		i++;
 	}
 	return (1);
 }
 
 /**
  * _strlen - returns the length of a string
- * @n: string to evaluate
+ * @s: string to evaluate
  *
  * Return: the length of the string
  */
-int _strlen(char *n)
+int _strlen(char *s)
 {
-	int x = 0;
+	int i = 0;
 
-	while (n[x] != '\0')
+	while (s[i] != '\0')
 	{
-		x++;
+		i++;
 	}
-	return (x);
+	return (i);
 }
 
 /**
@@ -59,7 +59,7 @@ void errors(void)
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
-	int len1, len2, len, x, carry, digit1, digit2, *result, k = 0;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
@@ -70,8 +70,8 @@ int main(int argc, char *argv[])
 	result = malloc(sizeof(int) * len);
 	if (!result)
 		return (1);
-	for (x = 0; x <= len1 + len2; x++)
-		result[x] = 0;
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
 	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
 		digit1 = s1[len1] - '0';
@@ -86,14 +86,14 @@ int main(int argc, char *argv[])
 		if (carry > 0)
 			result[len1 + len2 + 1] += carry;
 	}
-	for (x = 0; x < len - 1; x++)
+	for (i = 0; i < len - 1; i++)
 	{
-		if (result[x])
-			k = 1;
-		if (k)
-			_putchar(result[x] + '0');
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
 	}
-	if (!k)
+	if (!a)
 		_putchar('0');
 	_putchar('\n');
 	free(result);
