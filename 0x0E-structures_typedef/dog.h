074ac10df3e8a56e5a2dac5a62c51288992c9174
Mirimo1967 <vivmirimo@gmail.com>
2023-08-15 01:05:48 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,7 +7,7 @@
  * @age: Second member
  * @owner: Third member
  *
- * Description: Dog description
+ * Description: Longer description
  */
 struct dog
 {
@@ -17,15 +17,16 @@ struct dog
 };
 
 /**
- * dog_d - typedef for struct dog
+ * dog_t - typedef for struct dog
  */
-typedef struct dog dog_d;
+typedef struct dog dog_t;
 
-void init_dog(struct dog *o, char *name, float age, char *owner);
-void print_dog(struct dog *o);
-dog_d *new_dog(char *name, float age, char *owner);
-void free_dog(dog_d *o);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 
 #endif
+
